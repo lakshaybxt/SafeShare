b07653237a4e185ffe9c725bd14f2cded092cc7c
lakshaybxt <bishtlakshay14@gmail.com>
2025-06-07 23:40:25 +0530

    Added Email Service

---
+++ safeShare-be/src/main/java/com/lakshay/safeShare_be/service/EmailService.java
@@ -0,0 +1,7 @@
+package com.lakshay.safeShare_be.service;
+
+import jakarta.mail.MessagingException;
+
+public interface EmailService {
+    void sendVerificationEmail(String to, String subject, String text) throws MessagingException;
+}
---
+++ safeShare-be/src/main/java/com/lakshay/safeShare_be/service/impl/EmailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.lakshay.safeShare_be.service.impl;
+
+import com.lakshay.safeShare_be.service.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender emailSender;
+
+    @Override
+    public void sendVerificationEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text);
+        
+        emailSender.send(message);
+    }
+}
