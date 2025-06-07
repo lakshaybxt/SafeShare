e7d373194c3d176cbeb67b0221339faf9ac8ad5f
lakshaybxt <bishtlakshay14@gmail.com>
2025-06-07 12:08:03 +0530

    Added User Repo

---
+++ safeShare-be/src/main/java/com/lakshay/safeShare_be/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.lakshay.safeShare_be.repository;
+
+import com.lakshay.safeShare_be.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verification);
+}
