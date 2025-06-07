0fc6f9fd0e98f3000771494bb53f2ef6a8b259cf
lakshaybxt <bishtlakshay14@gmail.com>
2025-06-07 13:43:41 +0530

    Added Login Response

--- safeShare-be/src/main/java/com/lakshay/safeShare_be/configuration/SecurityConfig.java
+++ safeShare-be/src/main/java/com/lakshay/safeShare_be/configuration/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
-        
+
         return source;
     }
 }
---
+++ safeShare-be/src/main/java/com/lakshay/safeShare_be/response/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.lakshay.safeShare_be.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private String email;
+    private Long expiration ;
+}
