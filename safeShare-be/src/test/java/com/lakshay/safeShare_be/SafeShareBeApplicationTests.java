e8f989a4bce05db0c630d2413c9055e2dbf8e3a1
lakshaybxt <bishtlakshay14@gmail.com>
2025-06-07 08:17:48 +0530

    Initialize Backend

---
+++ safeShare-be/src/test/java/com/lakshay/safeShare_be/SafeShareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshay.safeShare_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeShareBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
